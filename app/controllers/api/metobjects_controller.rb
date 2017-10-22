2c1f4f8d4a7a4767652eea31c47e2d1d33385562
Janine Rosen <janinemrosen@gmail.com>
2017-10-22 11:41:08 -0400

    changing the component to fetch the hash of images rather than just one image

--- app/controllers/api/metobjects_controller.rb
+++ app/controllers/api/metobjects_controller.rb
@@ -2,6 +2,7 @@ class Api::MetobjectsController < ApplicationController
 	
 	def index
 		@metobjects = MetObject.where('1=1').order("RANDOM()").limit(50)
+		#@metobjects = MetObject.find(rand(1..MetObject.count))
 		render json: @metobjects
 	end
 
